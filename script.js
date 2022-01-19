a5b189aa722243028af4f388bc114b13125820aa
Joe O'Keefe <joeyokeefe5@gmail.com>
2022-01-19 12:25:21 -0500

    all

---
+++ script.js
@@ -0,0 +1,9 @@
+const choiceslist = ["rock", "paper", "scissors"];
+
+console.log(choiceslist);
+
+//function computerplay(randomchoice){
+    const choiceNumber = getRandomInt(3);
+    const choice = choiceslist[choicenumber];
+    console.log(choice);
+   // }
